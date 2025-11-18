f1162c8f4e68224d3020775c8cd9c49b6f97578c
Hélène Rizzon <helenerizzon3@gmail.com>
2025-11-18 21:29:48 +0100

    ParityChecker finished

--- src/javacore/chapter02/condition/exercise/ParityChecker.java
+++ src/javacore/chapter02/condition/exercise/ParityChecker.java
@@ -5,5 +5,24 @@ public class ParityChecker {
     public static void main(String[] args){
 
 
+        int number = 553;
+
+        System.out.println(number/2);
+        System.out.println(number%2);
+        System.out.println(number);
+
+        if ( number % 2 == 0) {
+            System.out.println("Le nombre donné est paire");
+        }
+        else {
+            System.out.println("Le nombre donné est impaire");
+        }
+
+
+        long number2 = 3333777799999L;
+
+        System.out.println(number2 % 2);
+
+
     }
 }
