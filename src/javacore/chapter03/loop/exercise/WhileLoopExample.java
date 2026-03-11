7a6052b11d8a7ddaac64bb51dbb7aec945d44b44
Hélène Rizzon <helenerizzon3@gmail.com>
2026-03-11 09:56:22 +0100

    WhileLoopExample Exercices finished

--- src/javacore/chapter03/loop/exercise/WhileLoopExample.java
+++ src/javacore/chapter03/loop/exercise/WhileLoopExample.java
@@ -4,15 +4,44 @@ public class WhileLoopExample {
 
     public static void main(String[] args){
 
-        int loopCounter = 1;
+//        int loopCounter = 1;
+//
+//        while(loopCounter <= 10){
+//
+//            loopCounter++;
+//
+//            System.out.println("Hello world");
+//        }
+//
+//        // Affichez les nombres de 1 à 10
+//        int number = 0;
+//
+//        while (number<10){
+//            number++;
+//            System.out.println(number);
+//        }
+//
+//        // Affichez une séquence en ordre décroissant
+//        int numberDec = 11;
+//
+//        while (numberDec>1){
+//            numberDec--;
+//            System.out.println(numberDec);
+//        }
 
-        while(loopCounter <= 1000){
+        // BONUS
+        int finBoucle = 20;
+        int numberBonus = 1;
 
-            loopCounter++;
-
-            System.out.println("Hello world");
+        while(true){
+            numberBonus ++;
+            if(numberBonus % 2 == 0){
+                System.out.println(numberBonus);
+            }
+            if (numberBonus == finBoucle) {
+                break;
+            }
         }
-
 
     }
 }
