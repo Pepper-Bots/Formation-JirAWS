afbb52b2020d3cf6d48f7af4b50ec2968ce98854
Hélène Rizzon <helenerizzon3@gmail.com>
2026-04-21 13:37:56 +0200

    Exercice ForLoopExample en cours

--- src/javacore/chapter03/loop/exercise/ForLoopExample_.java
+++ src/javacore/chapter03/loop/exercise/ForLoopExample_.java
@@ -4,6 +4,17 @@ public class ForLoopExample_ {
 
     public static void main(String[] args){
 
+        for(int number = 1; number <= 10; number ++){
+            System.out.println(number);
+        }
 
+        for(int number = 10; number >= 1; number --){
+            System.out.println(number);
+        }
+
+        String letter = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+        for(int i = 0; i < letter.length(); i++ ){
+            System.out.println(letter);
+        }
     }
 }
