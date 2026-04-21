85b2d00f23fe080e81f5d8403953aa6e3a102aac
Hélène Rizzon <helenerizzon3@gmail.com>
2026-04-21 22:06:13 +0200

    Exercice ForLoopExample en cours 2

--- src/javacore/chapter03/loop/exercise/ForLoopExample_.java
+++ src/javacore/chapter03/loop/exercise/ForLoopExample_.java
@@ -1,20 +1,26 @@
 package javacore.chapter03.loop.exercise;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ForLoopExample_ {
 
     public static void main(String[] args){
 
+        // Affichez les nombres de 1 à 10
         for(int number = 1; number <= 10; number ++){
             System.out.println(number);
         }
 
+        // Affichez les nombres de 10 à 1
         for(int number = 10; number >= 1; number --){
             System.out.println(number);
         }
 
-        String letter = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
-        for(int i = 0; i < letter.length(); i++ ){
-            System.out.println(letter);
+        // Affichez les lettres de A à Z
+        List<String> letter = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+        for (int i = 0; i < letter.size(); i++ ){
+            System.out.println(letter.get(i));
         }
     }
 }
