d901932b9ab07be431a4059961c00234905e0232
Hélène Rizzon <helenerizzon3@gmail.com>
2026-04-30 00:09:53 +0200

    ForLoopExample finishedd

--- src/javacore/chapter03/loop/exercise/ForLoopExample_.java
+++ src/javacore/chapter03/loop/exercise/ForLoopExample_.java
@@ -8,35 +8,30 @@ public class ForLoopExample_ {
     public static void main(String[] args){
 
 //        // Affichez les nombres de 1 à 10
-//        for(int number = 1; number <= 10; number ++){
-//            System.out.println(number);
-//        }
-//
+        for(int number = 1; number <= 10; number ++){
+            System.out.println(number);
+        }
+
 //        // Affichez une séquence en ordre décroissant
-//        for(int number = 10; number >= 1; number --){
-//            System.out.println(number);
-//        }
-//
+        for(int number = 10; number >= 1; number --){
+            System.out.println(number);
+        }
+
 //        // Affichez l'alphabet
-//        List<String> letter = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-//        for (int i = 0; i < letter.size(); i++ ){
-//            System.out.println(letter.get(i));
-//        }
+        List<String> letter = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+        for (int i = 0; i < letter.size(); i++ ){
+            System.out.println(letter.get(i));
+        }
 
         // Calculez une factorielle : Utilisez une boucle for pour calculer et afficher le résultat d'une factorielle.
         //Exemple d'affichage : La factorielle de 4 est 24
         // factorielle d'un entier naturel n est notée n! = produit des nb entiers strictement positifs inférieurs ou égaux à n
-
         // Factorielle de 5! = 5 * 4 * 3 * 2 * 1 = 120
-        // 6! = 6 * 5 * 4 * 3 * 2 * 1 =
         int n = 5;
         long factorielle = 1;
         for(int i = 1; i <= n; i++){
             factorielle *= i;
         }
         System.out.println("La factorielle de " + n + " est : " + factorielle);
-
-
     }
-
 }
