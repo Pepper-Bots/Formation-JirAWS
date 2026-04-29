1f191b34efac17ed7218e4bd6d954c89b9826082
Hélène Rizzon <helenerizzon3@gmail.com>
2026-04-30 00:07:45 +0200

    ForLoopExample finished

--- src/javacore/chapter03/loop/exercise/ForLoopExample_.java
+++ src/javacore/chapter03/loop/exercise/ForLoopExample_.java
@@ -7,20 +7,36 @@ public class ForLoopExample_ {
 
     public static void main(String[] args){
 
-        // Affichez les nombres de 1 à 10
-        for(int number = 1; number <= 10; number ++){
-            System.out.println(number);
-        }
+//        // Affichez les nombres de 1 à 10
+//        for(int number = 1; number <= 10; number ++){
+//            System.out.println(number);
+//        }
+//
+//        // Affichez une séquence en ordre décroissant
+//        for(int number = 10; number >= 1; number --){
+//            System.out.println(number);
+//        }
+//
+//        // Affichez l'alphabet
+//        List<String> letter = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+//        for (int i = 0; i < letter.size(); i++ ){
+//            System.out.println(letter.get(i));
+//        }
 
-        // Affichez les nombres de 10 à 1
-        for(int number = 10; number >= 1; number --){
-            System.out.println(number);
-        }
+        // Calculez une factorielle : Utilisez une boucle for pour calculer et afficher le résultat d'une factorielle.
+        //Exemple d'affichage : La factorielle de 4 est 24
+        // factorielle d'un entier naturel n est notée n! = produit des nb entiers strictement positifs inférieurs ou égaux à n
 
-        // Affichez les lettres de A à Z
-        List<String> letter = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-        for (int i = 0; i < letter.size(); i++ ){
-            System.out.println(letter.get(i));
+        // Factorielle de 5! = 5 * 4 * 3 * 2 * 1 = 120
+        // 6! = 6 * 5 * 4 * 3 * 2 * 1 =
+        int n = 5;
+        long factorielle = 1;
+        for(int i = 1; i <= n; i++){
+            factorielle *= i;
         }
+        System.out.println("La factorielle de " + n + " est : " + factorielle);
+
+
     }
+
 }
